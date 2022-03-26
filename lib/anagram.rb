5c2408bd798e2ab7dc4f6552aa063fe6c1f36484
lucasfarmwald <farmwaldlucas@gmail.com>
2022-03-25 21:46:21 -0500

    commit tree 462062b53aff4596965d29aa4aebfc4b5894c183

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :name
+    def initialize(word)
+        @name = word
+    end
+    def match(arrays)
+        arrays.select {|array| array.split("").sort == @name.split("").sort}
+    end
+end
